c08dc6c8cbc9bb31bfd5a9fe035941c7186431a6
ricassiocosta <riksiocosta@gmail.com>
2020-08-03 13:52:46 -0300

    teacher card created

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
 
 function TeacherList() {
@@ -24,6 +25,33 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/42079830?s=460&u=acba4bc2b5d0e97089748e4b787fd6f957460688&v=4" alt="avatar"/>
+            <div>
+              <strong>Ricássio Costa</strong>
+              <span>História das Revoluções</span>
+            </div>
+          </header>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            <br/>
+            Ipsum quasi eligendi temporibus repellat quam, quod, laborum veniam mollitia illo, placeat assumenda et? Praesentium a deserunt, non debitis facere voluptas possimus?
+          </p>
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 40,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   </>)
 }
