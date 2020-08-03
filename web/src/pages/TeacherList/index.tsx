932720bb9e1d443e1f550b10a0c3ec5bb70a7fed
ricassiocosta <riksiocosta@gmail.com>
2020-08-03 11:14:01 -0300

    study page initialized

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+import './styles.css'
 
 function TeacherList() {
   return(<>
-    <h1>Teachers</h1>
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis</strong>
+        </div>
+      </header>
+    </div>
   </>)
 }
 
