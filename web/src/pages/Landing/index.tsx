01305dc71ff3fee657ab7ecac7b5ca998fcde1cf
ricassiocosta <riksiocosta@gmail.com>
2020-08-03 10:48:19 -0300

    added Link component to prevent loadings

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
@@ -20,14 +21,14 @@ function Landing() {
         <img src={landingImg} alt="hero" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="estudar"/>
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="ensinar"/>
             Ensinar
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TeacherForm() {
   return(<>
-    <h1>Teachers</h1>
+    <h1>Give Classes</h1>
   </>)
 }
 
