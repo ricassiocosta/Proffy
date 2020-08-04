c7a81669f52e38745f6e8a49eb6dc0576ab5b7c6
ricassiocosta <riksiocosta@gmail.com>
2020-08-04 15:40:31 -0300

    creating classes

---
+++ server/src/routes.ts
@@ -0,0 +1,56 @@
+import express from 'express'
+import db from './database/connection'
+import convertHourToMinutes from './utils/convertHourToMinutes'
+
+const routes = express.Router()
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+routes.post('/classes', async (req, res) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = req.body
+
+  const insertedUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  })
+
+  const user_id = insertedUsersIds[0]
+
+  const insertedClassesIds = await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  })
+
+  const class_id = insertedClassesIds[0]
+
+  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+    return {
+      class_id,
+      week_day: scheduleItem.week_day,
+      from: convertHourToMinutes(scheduleItem.from),
+      to: convertHourToMinutes(scheduleItem.to)
+    }
+  })
+
+  await db('class_schedule').insert(classSchedule)
+
+  return res.send()
+})
+
+
+export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
-
-app.get('/users', (req, res) => {
-  return res.send('hello world!')
-})
+app.use(routes)
 
 app.listen(5000, () => {
   console.log('server is running')
