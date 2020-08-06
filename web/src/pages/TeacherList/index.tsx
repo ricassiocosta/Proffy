233c4176104a1f8541b4d1f4f745037cd2c7a6e1
ricassiocosta <riksiocosta@gmail.com>
2020-08-06 11:09:26 -0300

    teacher form schedule created

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,14 +4,42 @@ import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacherItem'
 import './styles.css'
 import Input from '../../components/Input'
+import Select from '../../components/Select'
 
 function TeacherList() {
   return(<>
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria"/>
-          <Input name="week_day" label="Dia da semana"/>
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'História', label: 'História'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Português', label: 'Português'},
+              {value: 'Química', label: 'Química'},
+              {value: 'Geografia', label: 'Geografia'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Filosofia', label: 'Filosofia'},
+              {value: 'Sociologia', label: 'Sociologia'},
+            ]}
+          />
+          <Select 
+            name="week_day" 
+            label="Dia da semana"
+            options={[
+              {value: '0', label: 'Domingo'},
+              {value: '1', label: 'Segunda-feira'},
+              {value: '2', label: 'Terça-feira'},
+              {value: '3', label: 'Quarta-feira'},
+              {value: '4', label: 'Quinta-feira'},
+              {value: '5', label: 'Sexta-feira'},
+              {value: '6', label: 'Sábado'},
+            ]}
+          />
           <Input type="time" name="time" label="Hora"/>
         </form>
       </PageHeader>
