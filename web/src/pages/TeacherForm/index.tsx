d95554f0a5dc8ba17a783e3eab9fc92ab161437f
ricassiocosta <riksiocosta@gmail.com>
2020-08-06 10:18:50 -0300

    teacher form footer style

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
+import Input from '../../components/Input'
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 function TeacherForm() {
   return(<>
@@ -14,20 +17,26 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
-
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp"/>
-          </div>
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
         </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria"/>
+          <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
       </main>
 
     </div>
