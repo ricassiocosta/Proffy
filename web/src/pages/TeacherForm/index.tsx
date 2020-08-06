1ff9a08f918338432bf017647cff9a2d6b48aaec
ricassiocosta <riksiocosta@gmail.com>
2020-08-06 10:34:29 -0300

    turned textarea into a component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import Input from '../../components/Input'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea'
 
 function TeacherForm() {
   return(<>
@@ -20,6 +21,7 @@ function TeacherForm() {
           <Input name="name" label="Nome completo"/>
           <Input name="avatar" label="Avatar"/>
           <Input name="whatsapp" label="Whatsapp"/>
+          <Textarea name="bio" label="Biografia"/>
         </fieldset>
         <fieldset>
           <legend>Sobre a aula</legend>
