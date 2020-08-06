e0a5f4d14b38fd2d9f6b02bf0f4ea18875bf3ebe
ricassiocosta <riksiocosta@gmail.com>
2020-08-06 14:02:43 -0300

    creating connection

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+  
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+  
   return(<>
     <article className="teacher-item">
       <header>
@@ -34,7 +42,12 @@ const TeacherItem:React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ {teacher.cost},00</strong>
         </p>
-        <a  href={`https://wa.me/${teacher.whatsapp}`}>
+        <a 
+          target="_blank" 
+          rel="noopener noreferrer"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="whatsapp"/>
           Entrar em contato
         </a>
