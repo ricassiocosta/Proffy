b749ccf3dfdcaa5318d952113a8ff6f80bd342b3
ricassiocosta <riksiocosta@gmail.com>
2020-08-06 11:25:50 -0300

    add new-schedule feature

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select:React.FunctionComponent<SelectProps> = ({ name, label, options, ...
   return (<>
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest}>
-        <option value="" disabled selected hidden>Selecione uma opção</option>
+      <select defaultValue="" id={name} {...rest}>
+        <option value="" disabled hidden>Selecione uma opção</option>
         {options.map(option => {
           return <option value={option.value} key={option.value}>{option.label}</option>
         })}
