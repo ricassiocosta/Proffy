c232b8fc8c1e32a06ce8051664acbe6c1c0cd91f
ricassiocosta <riksiocosta@gmail.com>
2020-08-07 14:23:27 -0300

    landing page started

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/pages/Landing';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<>
+    <Landing />
+    <StatusBar style="auto" />
+  </>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
