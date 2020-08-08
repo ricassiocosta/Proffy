3bb75f0122f7615204e4cc93e9c8b0edf024f96c
ricassiocosta <riksiocosta@gmail.com>
2020-08-08 14:19:36 -0300

    proffys listing

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png'
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png'
+import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+
+import styles from './styles'
+
+function TeacherItem() {
+  return(
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image source={{uri: 'https://github.com/ricassiocosta.png'}} style={styles.avatar} />
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Ricassio Costa</Text>
+          <Text style={styles.subject}>História</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        {'\n'}{`\n`}
+        A placeat debitis quaerat rem et necessitatibus nobis sunt, sed fuga accusamus ipsum repellat deleniti qui laboriosam totam blanditiis enim quo temporibus.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'  '}
+          <Text style={styles.priceValue}>R$ 20,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default TeacherItem
--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -3,12 +3,30 @@ import { View } from 'react-native'
 
 import styles from './styles'
 import PageHeader from '../../components/PageHeader'
+import { ScrollView } from 'react-native-gesture-handler'
+import TeacherItem from '../../components/TeacherItem'
 
 
 function Favorites() {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>  
   )
 }
